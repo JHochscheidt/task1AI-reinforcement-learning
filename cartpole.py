465058bcc5d20820990fe32210940c54c2e27955
jacksonhh <jackson94h@gmail.com>
2018-04-29 18:07:25 -0300

    Arrumada a funcao de learning

--- cartpole.py
+++ cartpole.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 
-#!/usr/bin/python2
+#!/usr/bin/python2.7
 import numpy as np
 import pickle as pickle
 #import tensorflow as tf
@@ -12,27 +12,31 @@ import sys
 
 env = gym.make('CartPole-v0')
 actions = range(env.action_space.n) # retorna quantidade de ações
-number_episodes = 5000
+number_episodes = 10000
 reward_sum = 0
 number_steps = 200
 number_training = 20
 
 #Definicao do dicionario
 qtable = {}
-epsilon = 0.1
-gamma = 0.8
-alpha = 0.1
+epsilon = 0.5
+gamma = 1   # gama : é o fator de desconto, também definido entre 0 e 1. 
+            # Isso modela o fato de que recompensas futuras valem menos do que recompensas imediatas.
+
+alpha = 0.9 # alpha : é a taxa de aprendizado, definida geralmente entre 0 e 1. 
+            # Configurá-la como 0 significa que os valores Q nunca são atualizados, portanto, nada é aprendido. 
+            # Definir alfa para um valor alto como 0,9 significa que o aprendizado pode ocorrer rapidamente.
 
 #Definição do Domínio das variáveis
 interval_car = 2.5
 interval_vel_car = 5
-interval_angle = 0.3
+interval_angle = 15
 interval_vel_angle = 5
 
-domain_car = 5
-domain_vel_car = 5
-domain_angle = 5
-domain_vel_angle = 5
+domain_car = 20
+domain_vel_car = 40
+domain_angle = 60
+domain_vel_angle = 40
 
 last_time_steps = np.ndarray(0)
 
@@ -40,6 +44,10 @@ car_positions = pandas.cut([-interval_car, interval_car], bins = domain_car, ret
 vels_car = pandas.cut([-interval_vel_car, interval_vel_car], bins = domain_vel_car, retbins=True)[1][1:]
 angle_positions = pandas.cut([-interval_angle, interval_angle], bins = domain_angle, retbins=True)[1][1:]
 vels_angle = pandas.cut([-interval_vel_angle, interval_vel_angle], bins = domain_vel_angle, retbins=True)[1][1:]
+
+
+
+
 
 # só cria o dicionario padrao
 def create_qtable():
@@ -53,24 +61,18 @@ def create_qtable():
                     angle =  np.around(angle, 5) # discretiza(angle, angle_positions)
                     vel_angle = np.around(vel_angle, 5) #discretiza(vel_angle, vels_angle)
                     state = '{' + str(car) + ',' + str(vel_car) + ',' + str(angle) + ',' + str(vel_angle) + '}'
-                    qtable[state] = (1.0 ,1.0)
+                    qtable[state] = (1 ,1)
     return qtable
 
 # salva o dicionario (qtable) no .txt
 def save_qtable(qtable, name_file):
-    #name_file = input("Salvar arquivo como... (file name)\n")
-    #name_file = name_file
-
-    # = open(name_file, 'w')
-    
-    with open(name_file, 'w') as f:
+   with open(name_file, 'w') as f:
         json.dump(qtable, f) 
 
 # carrega o dicionario (qtable) do .txt
-def load_qtable(name_file):
-    
+def load_qtable(name_file):   
     try:
-        with open(name_file) as f:
+        with open(name_file, 'r') as f:
             return json.load(f)
     except:
         qtable = create_qtable()
@@ -109,15 +111,22 @@ def escolhe_action(state):
     action = actions[i]
     return action, maxQ
 
+
+
 # atualiza a qtable
-def learnQ(currrent_state, action, reward, value):
-    oldvalue = qtable[current_state][action]
-    qtable[current_state][action] = np.around(oldvalue + alpha * ( value - oldvalue), 5)
-    
-# esta aprendendo a mlk
 def learning(current_state, next_state, action, reward):
-    ind_max_action, max_Qnew = escolhe_action(current_state)
-    learnQ(current_state, action, reward, gamma * max_Qnew)
+    
+    qtable[current_state][action] = np.around(qtable[current_state][action] + alpha *(reward + gamma*(max(qtable[next_state]) - qtable[current_state][action])), 5)
+
+       
+    #print 'Qold[', qtable[current_state] , '][' , action , ']=' , oldvalue , '\n'
+    #print 'Qnew[' , current_state , '][' , action , ']=' , qtable[current_state][action] , '\n'
+
+
+def print_qtable(qtable):
+    for key in qtable:
+        print 'qtable[' + key + ']', qtable[key] , '\n'
+
 
 if __name__ == "__main__":
     
@@ -129,33 +138,39 @@ if __name__ == "__main__":
     
     
     name_file = sys.argv[1]
-    print name_file
-
-    
+        
     qtable = {}
     qtable = load_qtable(name_file)
     for training in range(number_training):
-        print training
+        #print 'training', training
         env.reset()
         for episode in range(number_episodes):
+            #print 'episode', episode
             observation = env.reset()
-
-            #discretizar observation de modo a criar um estado valido
-            current_state = construi_estado(observation)
 
             for step in range(number_steps):
                 #env.render()
+                #print 'step', step
+
+
+                #discretizar observation de modo a criar um estado valido
+                current_state = construi_estado(observation)
+
                 #escolhe a melhor acao para o estado atual
                 action, value_action  = escolhe_action(current_state)
                 observation, reward, done, info = env.step(action)
                 next_state = construi_estado(observation)
-
+                
                 #executa esta acao 
                 # ????para saber qual é a melhor acao para o estaod atual?????
                 #depois disso, pega o novo observation da acao tomada acima para saber qual é o proximo estado
                 #aqui esta em S
                 #observation, reward, done, info = env.step(action)
                 # aqui esta em S'
+
+                #print_qtable(qtable)
+
+
                 if not (done):
                     # chama para aprender novamente, dado o estado atual e o proximo estado
                     # atualiza estado atual = proximo estado
