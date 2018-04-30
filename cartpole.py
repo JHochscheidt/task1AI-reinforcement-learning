e3ea3f0267a47b6dbeb93a7746c76c692f88f6e2
jacksonhh <jackson94h@gmail.com>
2018-04-29 21:38:25 -0300

    colocado uma tentativa de escolher com uma certa probabilidade <epsilon> uma acao aleatoria, para nao pegar a mlhor sempre

--- cartpole.py
+++ cartpole.py
@@ -9,18 +9,19 @@ import gym
 import pandas
 import json
 import sys
+import random
 
 env = gym.make('CartPole-v0')
 actions = range(env.action_space.n) # retorna quantidade de ações
-number_episodes = 10000
+number_episodes = 500
 reward_sum = 0
 number_steps = 200
-number_training = 20
+number_training = 1
 
 #Definicao do dicionario
 qtable = {}
-epsilon = 0.5
-gamma = 1   # gama : é o fator de desconto, também definido entre 0 e 1. 
+epsilon = 0.2
+gamma = 0.1 # gama : é o fator de desconto, também definido entre 0 e 1. 
             # Isso modela o fato de que recompensas futuras valem menos do que recompensas imediatas.
 
 alpha = 0.9 # alpha : é a taxa de aprendizado, definida geralmente entre 0 e 1. 
@@ -28,15 +29,15 @@ alpha = 0.9 # alpha : é a taxa de aprendizado, definida geralmente entre 0 e 1.
             # Definir alfa para um valor alto como 0,9 significa que o aprendizado pode ocorrer rapidamente.
 
 #Definição do Domínio das variáveis
-interval_car = 2.5
-interval_vel_car = 5
-interval_angle = 15
-interval_vel_angle = 5
+interval_car = 2.4
+interval_vel_car = 6
+interval_angle = 12.4
+interval_vel_angle = 6 
 
-domain_car = 20
-domain_vel_car = 40
-domain_angle = 60
-domain_vel_angle = 40
+domain_car = 2
+domain_vel_car = 4
+domain_angle = 10
+domain_vel_angle = 1
 
 last_time_steps = np.ndarray(0)
 
@@ -45,6 +46,10 @@ vels_car = pandas.cut([-interval_vel_car, interval_vel_car], bins = domain_vel_c
 angle_positions = pandas.cut([-interval_angle, interval_angle], bins = domain_angle, retbins=True)[1][1:]
 vels_angle = pandas.cut([-interval_vel_angle, interval_vel_angle], bins = domain_vel_angle, retbins=True)[1][1:]
 
+# print 'car_positions', car_positions
+# print 'vels_car', vels_car
+# print 'angle_positions', angle_positions
+# print 'vels_angle', vels_angle
 
 
 
@@ -56,10 +61,10 @@ def create_qtable():
         for vel_car in vels_car:
             for angle in angle_positions:
                 for vel_angle in vels_angle:                    
-                    car = np.around(car, 5)
-                    vel_car = np.around(vel_car, 5) # discretiza(vel_car, vels_car)
-                    angle =  np.around(angle, 5) # discretiza(angle, angle_positions)
-                    vel_angle = np.around(vel_angle, 5) #discretiza(vel_angle, vels_angle)
+                    car = np.around(car, 3)
+                    vel_car = np.around(vel_car, 3) # discretiza(vel_car, vels_car)
+                    angle =  np.around(angle, 3) # discretiza(angle, angle_positions)
+                    vel_angle = np.around(vel_angle, 3) #discretiza(vel_angle, vels_angle)
                     state = '{' + str(car) + ',' + str(vel_car) + ',' + str(angle) + ',' + str(vel_angle) + '}'
                     qtable[state] = (1 ,1)
     return qtable
@@ -88,21 +93,44 @@ def discretiza(valor_observation, intervalo_discretizado):
 
 # transforma a "contatenacao" dos valores do observation em um estado valido no dicionario (qtable)
 def construi_estado(observation):
-    car = discretiza(observation[0], car_positions)
-    vel_car = discretiza(observation[1], vels_car)
-    angle = discretiza(observation[2], angle_positions)
-    vel_angle = discretiza(observation[3], vels_angle)
+    car = np.around(observation[0], 3)
+    vel_car = np.around(observation[1], 3)
+    angle = np.around(observation[2], 3)
+    vel_angle = np.around(observation[3], 3)
+
+    #print 'around{', car, vel_car, angle, vel_angle, '}'
+    
+    car = discretiza(car, car_positions)
+    vel_car = discretiza(vel_car, vels_car)
+    angle = discretiza(angle, angle_positions)
+    vel_angle = discretiza(vel_angle, vels_angle)
+    
+    #print 'discretize{', car, vel_car, angle, vel_angle, '}'
+
     car = car_positions[car] 
     vel_car = vels_car[vel_car]
     angle = angle_positions[angle]
     vel_angle = vels_angle[vel_angle]
-    state = '{' + str( np.around(car, 5)) + ',' + str(np.around(vel_car, 5)) + ',' + str(np.around(angle,5)) + ',' + str(np.around(vel_angle, 5)) + '}' 
+
+    #print 'value{', car, vel_car, angle, vel_angle, '}'
+
+    state = '{' + str(np.around(car,3)) + ',' + str(np.around(vel_car, 3)) + ',' + str(np.around(angle, 3)) + ',' + str(np.around(vel_angle, 3)) + '}' 
     return state
 
 # escolha uma acao dado o estado
 def escolhe_action(state):
     q = qtable[state]
     maxQ = max(q)
+
+    #tenta probabilisticamente escolher um estado aleatorio
+    if np.random.random_sample() < epsilon:
+        minQ = min(q)
+        mag = max(abs(minQ), abs(maxQ))
+        # add random values to all the actions, recalculate maxQ
+        q = [q[i] + np.random.random_sample() * mag - .5 * mag for i in range(len(actions))] 
+        maxQ = max(q)
+
+
     if q.count(maxQ) > 1:
         best = [i for i in range(len(actions)) if q[i] == maxQ]
         i = np.random.choice(best)
@@ -115,41 +143,41 @@ def escolhe_action(state):
 
 # atualiza a qtable
 def learning(current_state, next_state, action, reward):
-    
-    qtable[current_state][action] = np.around(qtable[current_state][action] + alpha *(reward + gamma*(max(qtable[next_state]) - qtable[current_state][action])), 5)
 
-       
+    qtable[current_state][action] = np.around(qtable[current_state][action] + alpha *(reward + gamma*(max(qtable[next_state]) - qtable[current_state][action])), 5)
     #print 'Qold[', qtable[current_state] , '][' , action , ']=' , oldvalue , '\n'
     #print 'Qnew[' , current_state , '][' , action , ']=' , qtable[current_state][action] , '\n'
 
 
+#printa a qtable diferente
 def print_qtable(qtable):
     for key in qtable:
         print 'qtable[' + key + ']', qtable[key] , '\n'
 
-
+#main
 if __name__ == "__main__":
+
+    try:
+        name_file = sys.argv[1]
     
-    """
-    #chama somente uma vez para criar a qtable
-    #chama caso ainda nao exista o arquivo com a qtable
-    #qtable = create_qtable()  #save_qtable(qtable)
-    """
-    
-    
-    name_file = sys.argv[1]
-        
+    except:
+        print 'Passe como parametro o nome do arquivo que salva a Qtable'
+        exit(0)
+
     qtable = {}
     qtable = load_qtable(name_file)
+
+    #print_qtable(qtable)
+
     for training in range(number_training):
-        #print 'training', training
+        print 'training', training
         env.reset()
         for episode in range(number_episodes):
             #print 'episode', episode
             observation = env.reset()
 
             for step in range(number_steps):
-                #env.render()
+                env.render()
                 #print 'step', step
 
 
@@ -187,5 +215,8 @@ if __name__ == "__main__":
         print("Best score: {:0.2f}".format(max(l)))
         print("Desvio padrao: {:0.2f}".format(np.std(l)))
     save_qtable(qtable,name_file)
+
+    #print 'after \n\n'
+    #print_qtable(qtable)
        
 
