844322e6e8e273f542185ece0609de84011f0692
jacksonhh <jackson94h@gmail.com>
2018-05-01 18:34:00 -0300

    inserido funcao que verifica se existem 100 episodios seguidos com media >= 195.0

--- cartpole.py
+++ cartpole.py
@@ -13,7 +13,7 @@ interval_vel_car = 6
 interval_angle = 13
 interval_vel_angle = 6 
 #Definição do Domínio das variáveis
-domain_car = 1
+domain_car = 10
 domain_vel_car = 10
 domain_angle = 10
 domain_vel_angle = 10
@@ -67,11 +67,6 @@ def discretize(valor_observation, intervalo_discretizado):
 
 # transforma a "concatenacao" dos valores do observation em um estado valido no dicionario (qtable)
 def build_state(observation):
-    # car = np.around(observation[0], around)
-    # vel_car = np.around(observation[1], around)
-    # angle = np.around(observation[2], around)
-    # vel_angle = np.around(observation[3], around)
-
     car = discretize(observation[0], car_positions)
     vel_car = discretize(observation[1], vels_car)
     angle = discretize(observation[2], angle_positions)
@@ -89,18 +84,7 @@ def build_state(observation):
 def action_choice(state):
     q = qtable[state]
     maxQ = max(q)
-    #tenta "probabilisticamente" escolher um estado aleatorio
-    if np.random.random_sample() < epsilon:
-        minQ = min(q)
-        mag = max(abs(minQ), abs(maxQ))
-        # add random values to all the actions, recalculate maxQ
-        q = [q[i] + np.random.random_sample() * mag - .5 * mag for i in range(len(actions))] 
-        maxQ = min(q)
-    if q.count(maxQ) > 1:
-        best = [i for i in range(len(actions)) if q[i] == maxQ]
-        i = np.random.choice(best)
-    else:
-        i = q.index(maxQ)
+    i = q.index(maxQ)
     action = actions[i]
     return action, maxQ
 
@@ -109,6 +93,16 @@ def learning(current_state, next_state, action, reward):
     qtable[current_state][action] = \
         np.around(qtable[current_state][action] + \
         alpha *(reward + gamma*(max(qtable[next_state])) - qtable[current_state][action]), around)
+
+#verifica media de 100 valores consecutivos
+def average_hundred_consecutive_score(scores):
+    if len(scores) >= 100: #se vetor é maior que 100
+        for i in range(len(scores)):
+            if(len(scores[i:]) >= 100): #se vetor é maior que sem
+                media = scores[i:i+100].mean()
+                if media >= 195.0:
+                    return True, i
+    return False, len(scores)
 
 #main
 if __name__ == "__main__":
@@ -123,18 +117,18 @@ if __name__ == "__main__":
     """"""""""""""""""""""""""""""""
     """  PARAMETROS TREINAMENTO  """
     """"""""""""""""""""""""""""""""
-    number_episodes = 100   #numero de episodios
+    number_episodes = 200   #numero de episodios
     reward_sum = 0          #somatorio das recompensas
     number_steps = 200      #numero de passos                                           #este valor nao pode ser alterado
      
-    average_reward = 195    #media das recompensas ao final de todos os episodios       #este numero nao pode ser alterado
+    average_reward = 195                #media das recompensas ao final de todos os episodios       #este numero nao pode ser alterado
     #last_time_steps = np.ndarray(0)    #array que guarda quantos passos ocorreram em cada episodio
     last_time_steps = np.ndarray(0)     #array que guarda quantos passos ocorreram em cada episodio. 
     reward_steps = np.ndarray(0)        #array que guarda a recompensa total de cada episodio
     episodes = np.ndarray(0)            #array utilizado apenas para montar os graficos
-    epsilon = 0.2   # valor utilizado para escolher valor probabilisticamente
-    gamma = 0.90     # fator de desconto[0,1]. Define se recompensas futuras valem  valem menos do que recompensas imediatas.
-    alpha = 0.6     # taxa de aprendizado[0,1]. --> 0 significa que os valores Q nunca são atualizados, portanto, nada é aprendido. 
+
+    gamma = 0.9     # fator de desconto[0,1]. Define se recompensas futuras valem  valem menos do que recompensas imediatas.
+    alpha = 0.5     # taxa de aprendizado[0,1]. --> 0 significa que os valores Q nunca são atualizados, portanto, nada é aprendido. 
                     #                           --> Definir para um valor alto como 0,9 significa que o aprendizado pode ocorrer rapidamente.
 
     """"""""""""""""""""""""""""""""
@@ -146,7 +140,7 @@ if __name__ == "__main__":
     qtable = load_qtable(name_file)
 
     for episode in range(number_episodes):
-        print 'episode', episode
+        #print 'episode', episode
         episodes = np.append(episodes, episode)
         observation = env.reset()
         reward_sum = 0
@@ -159,12 +153,12 @@ if __name__ == "__main__":
             next_state = build_state(observation)
 
             if done:
-                if step != 199:
+                if step != (number_steps-1):
                     learning(current_state, next_state, action, -10) #só atualiza o estado que fez com que terminasse o episodio com uma recompensa negativa
                     reward_sum += -10 
                 reward_steps = np.append(reward_steps,reward_sum)      #adiciona recompensa ao vetor de recompensas
                 last_time_steps = np.append(last_time_steps, step+1)#adiciona valor do ultimo passo do episodio ao vetor
-                print("Episode finished after {} timesteps".format(step+1))
+                #print("Episode finished after {} timesteps".format(step+1))
                 break
             else:
                 reward_sum += reward
@@ -174,10 +168,6 @@ if __name__ == "__main__":
         #fim laço step
     #fim laço episode          
 
-    """ 
-    GRAFICOS 
-    """
-    
     avg_rw_all_episodes = reward_steps.mean()  #media de recompensa entre todos os episodios
     avg_last_time_steps = last_time_steps.mean() #media de passos entre todos os episodios
     std_dev_last_time_steps = np.std(last_time_steps) #desvio padrao
@@ -188,26 +178,43 @@ if __name__ == "__main__":
     avg_rw_all_ep = np.ndarray(number_episodes)
     avg_rw_all_ep[:] = avg_rw_all_episodes
 
+    solved, i = average_hundred_consecutive_score(last_time_steps)
+    if solved:
+        print '[' + str(i) + '|' + str(i+100) + ']\n' + str(last_time_steps[i:i+100]) + '\n'
+
+    """ 
+    GRAFICOS 
+    """
     fig = plt.figure() #figura em que serão desenhados os gráficos
     rect = fig.patch
     ax1 = fig.add_subplot(1,1,1) #figura terá apenas 1 gráfico
     ax1.set_title('Resultado treinamento')  #Título do gráfico
     ax1.set_xlabel('episode')               #Eixo X
     ax1.set_ylabel('step')                  #Eixo y
-    
-    
-
+   
     ax1.plot(episodes, last_time_steps , color='r', label='Qt steps') #plota o ultimo passo de cada episodio
-    ax1.plot(episodes, avg_rw_all_ep, color='g', label='Avg reward all') #plota a média de recompensa entre todos os episódios
     ax1.plot(episodes, avg_all_steps, color='b', label='Avg all episodes') #plota a média entre todos os episódios
+    if solved:
+        avg_best_hundred_consecutive_score = last_time_steps[i:i+100].mean()
+        avg_best_hd_con_score = np.ndarray(number_episodes)
+        avg_best_hd_con_score[i:i+100] = avg_best_hundred_consecutive_score
+        ax1.plot(episodes[i:i+100], avg_best_hd_con_score[i:i+100], color='g', label='Avg best 100 consecutives') #plota a média entre 100 melhores os episódios
     ax1.legend(loc='upper left')
 
     plt.show()
 
+    #last_time_steps.sort()
 
     print("\nAverage Overall score: {:0.2f}".format(avg_last_time_steps))
-    print("Average Reward Overall score: {:0.2f}".format(avg_rw_all_episodes))
     print("Best score: {:0.2f}".format(max(last_time_steps)))
     print("Standard Deviation: {:0.2f}".format(np.std(last_time_steps)))
+    
+
+
+    
+        
+
+    
+    
     save_qtable(qtable,name_file)
 
